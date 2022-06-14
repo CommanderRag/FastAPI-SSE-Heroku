71570b51048e82e04afd95429dbc0388fd006a15
CommanderRag <treetree3693@gmail.com>
2022-06-14 09:22:53 +0530

    fixes

--- sender.py
+++ sender.py
@@ -5,5 +5,5 @@ while True:
     message = input("Enter message to send:")
     if message == '':
         break
-    msg_json = {'message': message}.
+    msg_json = {'message': message}
     requests.post('http://fastapi-sse.herokuapp.com/generate', json=msg_json, headers={'Authorization': 'za-warudo', 'Content-Type': 'application/json'})
