11a16cd988b74bc4a73176277ebc94d4df9586d5
CommanderRag <treetree3693@gmail.com>
2022-07-07 06:42:55 +0530

    push for heroku

--- sender.py
+++ sender.py
@@ -6,4 +6,4 @@ while True:
     if message == '':
         break
     msg_json = {'message': message}
-    requests.post('https://fastapi-sse-heroku.naxeq.repl.co/generate', json=msg_json, headers={'Authorization': 'za-warudo', 'Content-Type': 'application/json'})
+    requests.post('http://fastapi-sse.herokuapp.com/generate', json=msg_json, headers={'Authorization': 'za-warudo', 'Content-Type': 'application/json'})
